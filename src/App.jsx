c84b95ddf844ad0034b21dd28dba4dcda1e8f7cb
Bethq321 <bethq321@LaptopBeth>
2023-12-04 17:39:32 -0600

    completed project without additional CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,56 @@
-import { useState } from 'react'
+
+import {puppyList} from './data.js'
 import './App.css'
+import { useState } from 'react' 
 
 function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
-  return (
-    <div></div>
+  console.log(puppies)
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId)
+  console.log(featuredPup)
+
+  // function featuredPup (id) {
+  //   const selectedPup = puppies.find((pup) => {
+  //     return pup.id === id
+  //   })
+  // }
+
+  return (    
+    <div className="App">
+
+      {puppies.map((puppy) => {
+          return( 
+            <p key={puppy.id} onClick={()=>{setFeatPupId(puppy.id)}}>{puppy.name}</p>
+          )
+        })}
+
+      { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      
+
+      {/* { featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )} */}
+
+
+
+    </div>
   )
 }
 
